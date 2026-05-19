e411c446169cd1406c917e775ab7656f14bcce4e
Nzayisenga Vital <vitalnzayisenga301@gmail.com>
2026-05-19 10:21:58 +0200

    fix static files on Render

--- civil/settings.py
+++ civil/settings.py
@@ -123,12 +123,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'civilengineering/static',
+]
 
 STATICFILES_STORAGE = (
     'whitenoise.storage.CompressedManifestStaticFilesStorage'
 )
-
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
